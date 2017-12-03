4e4cced89c6bd9e2d1ac646f30c0e173baedddb6
natfaulk <nfaulk@hotmail.co.uk>
2017-12-03 20:56:17 +1300

    draws path as is searching
    pathfinding massively slowed down so can be seen drawing

--- inc/constants.hpp
+++ inc/constants.hpp
@@ -1,8 +1,8 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
-const int MAP_WIDTH = 75;
-const int MAP_HEIGHT = 40;
+const int MAP_WIDTH = 200;
+const int MAP_HEIGHT = 120;
 #include <SFML/Graphics/Color.hpp>
 
 // ----------------------------------------------------------------------------
@@ -21,18 +21,18 @@ enum MapTiles {FLOOR, WALL, SRC, DEST, CUR_POS, SEARCHED, BEST};
 // ----------------------------------------------------------------------------
 // SFML Graphics
 // ----------------------------------------------------------------------------
-const int TILE_SIZE = 25;
+const int TILE_SIZE = 10;
 const int SCREEN_WIDTH = MAP_WIDTH * TILE_SIZE;
 const int SCREEN_HEIGHT= MAP_HEIGHT * TILE_SIZE;
 static const char* WINDOW_TITLE = "A STAR SFML";
 
 const sf::Color FLOOR_COLOR = sf::Color::White;
 const sf::Color WALL_COLOR = sf::Color::Red;
-const sf::Color SRC_COLOR = sf::Color::Blue;
-const sf::Color DEST_COLOR = sf::Color::Yellow;
+const sf::Color SRC_COLOR = sf::Color::Yellow;
+const sf::Color DEST_COLOR = sf::Color::Blue;
 const sf::Color CURRENT_POS_COLOR = sf::Color::Black;
-const sf::Color SEARCHED_COLOR = sf::Color::Cyan;
-const sf::Color BESTPATH_COLOR = sf::Color::Green;
+const sf::Color SEARCHED_COLOR = sf::Color(150, 150, 150);
+const sf::Color BESTPATH_COLOR = sf::Color::Black;
 
 // ----------------------------------------------------------------------------
 // Mapgen
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@
 #include "map.hpp"
 #include "utils.hpp"
 
+int frameCount = 0;
+const int FRAME_DIVIDER = 60 / 5;
+bool done = false;
+
 int main(int argc, char const *argv[]) {
   /* code */
   UTIL_RandSeed();
@@ -29,9 +33,13 @@ int main(int argc, char const *argv[]) {
 
   // MAP_Gen();
 
-  astar(&src, &dest);
+  // astar(&src, &dest);
   // ASTAR_PrintOpen();
-  MAP_Print();
+  // MAP_Print();
+
+  ASTAR_Begin(&src, &dest);
+  MAP_SetData(&src, SRC);
+  MAP_SetData(&dest, DEST);
 
   while (window.isOpen())
   {
@@ -41,20 +49,26 @@ int main(int argc, char const *argv[]) {
         if (event.type == sf::Event::Closed) window.close();
         if (event.type == sf::Event::KeyPressed) {
           if (event.key.code == sf::Keyboard::R) {
-              MAP_Clear();
-              MAP_GenRandom();
+            done = false;
+            MAP_Clear();
+            MAP_GenRandom();
 
-              int xpos = UTIL_RandBetween(1, MAP_WIDTH - 2);
-              int ypos = UTIL_RandBetween(1, MAP_HEIGHT - 2);
-              src = Point(xpos, ypos);
+            xpos = UTIL_RandBetween(1, MAP_WIDTH - 2);
+            ypos = UTIL_RandBetween(1, MAP_HEIGHT - 2);
+            src = Point(xpos, ypos);
 
-              xpos = UTIL_RandBetween(1, MAP_WIDTH - 2);
-              ypos = UTIL_RandBetween(1, MAP_HEIGHT - 2);
-              dest = Point(xpos, ypos);
+            xpos = UTIL_RandBetween(1, MAP_WIDTH - 2);
+            ypos = UTIL_RandBetween(1, MAP_HEIGHT - 2);
+            dest = Point(xpos, ypos);
 
-              // MAP_Gen();
+            ASTAR_Begin(&src, &dest);
+            MAP_SetData(&src, SRC);
+            MAP_SetData(&dest, DEST);
+          }
 
-              astar(&src, &dest);
+          if (event.key.code == sf::Keyboard::P) {
+            ASTAR_Test();
+            while(1);
           }
         }
         // if (event.type == sf::Event::MouseButtonPressed) {
@@ -63,8 +77,30 @@ int main(int argc, char const *argv[]) {
         // }
     }
 
-    MAP_Draw(window);
-    window.display();
+    if (frameCount++ >= FRAME_DIVIDER)
+    {
+      frameCount = 0;
+      if (!ASTAR_Step())
+      {
+        MAP_SetData(ASTAR_LatestSearched(), SEARCHED);
+      }
+      else if(!done)
+      {
+        Point nextPoint = ASTAR_PathNextPoint();
+        while(nextPoint.x >= 0)
+        {
+          MAP_SetData(&nextPoint, BEST);
+          nextPoint = ASTAR_PathNextPoint();
+        }
+        MAP_SetData(&src, SRC);
+        MAP_SetData(&dest, DEST);
+        done = true;
+      }
+
+      MAP_Draw(window);
+      window.display();
+    }
+
 
   }
 
